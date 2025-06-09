90b30d5cb8a63bc527cffbaf075d83739f767958
Anu <anupama12sg@gmail.com>
2025-06-09 10:21:17 +0530

    chore: added timeout and different heart sizes to the trail

--- 1. heart-trail-animation/index.js
+++ 1. heart-trail-animation/index.js
@@ -6,5 +6,11 @@ bodyElement.addEventListener("mousemove", (event) => {
     const spanElement = document.createElement("span");
     spanElement.style.left = xPos + "px";
     spanElement.style.top = yPos + "px";
+    const size = Math.random() * 100;
+    spanElement.style.width = size + "px";
+    spanElement.style.height = size + "px";
     bodyElement.appendChild(spanElement);
+    setTimeout(() => {
+        spanElement.remove();
+    }, 3000);
 });
