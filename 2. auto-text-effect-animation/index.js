fc8cec25ce455f88e81796dccfca1e6858ada968
Anu <anupama12sg@gmail.com>
2025-06-11 16:19:56 +0530

    chore: added careers list

---
+++ 2. auto-text-effect-animation/index.js
@@ -0,0 +1,7 @@
+const containerElement = document.querySelector(".container")
+
+const careers = ["Engineer", "Freelancer", "Asst. Professor", "Artist", "All Rounder"]
+
+containerElement.innerHTML = `
+<h1>I am a Engineer.</h1>
+`
