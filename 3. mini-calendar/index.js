62ff77d9dbac85132aba4bf49f7b3e70275098ab
Anu <anupama12sg@gmail.com>
2025-06-13 15:55:11 +0530

    chore: created 4 const for each id

---
+++ 3. mini-calendar/index.js
@@ -0,0 +1,7 @@
+const monthName = document.getElementById("month-name")
+
+const dayName = document.getElementById("day-name")
+
+const dayNum = document.getElementById("day-number")
+
+const year = document.getElementById("year")
