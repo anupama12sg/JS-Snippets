efae15d50edeaf97f8cfb82a558358cdce74ff26
Anu <anupama12sg@gmail.com>
2025-06-19 11:57:51 +0530

    chore: created object to add other testimonials

---
+++ 7. testimonial slider/index.js
@@ -0,0 +1,19 @@
+const testimonial = [
+    {
+        name: "Emily Johnson",
+        photoUrl: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        text: "Apple's McBook is a must-have for anyone seeking an efficient & innovative solution. The sleek design & powerful performance sets it apart from the rest."
+    },
+    {
+        name: "Sarah Reynolds",
+        photoUrl: "https://plus.unsplash.com/premium_photo-1670071482460-5c08776521fe?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        text: "iPhone exceeded all my expectations. The sleek design and advanced features make it a standout product."
+    },
+    {
+        name: "Georgia Miller",
+        photoUrl: "https://plus.unsplash.com/premium_photo-1675626487177-c3d2f8d9ccf7?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        text: "Since purchasing my new iPad from Apple, my productivity has soared. The sleek design and user-friendly interface make it a joy to use for both work and play. "
+    }
+
+
+]
