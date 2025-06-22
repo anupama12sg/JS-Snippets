268b4b273c4c36a31e1c93ca3decc1a028e17b28
Anu <anupama12sg@gmail.com>
2025-06-22 18:29:57 +0530

    chore: fucntion updateClock()

--- 9. digital-clock/index.js
+++ 9. digital-clock/index.js
@@ -2,3 +2,10 @@ const hourEl = document.getElementById("hour")
 const minutesEl = document.getElementById("minutes")
 const secondsEl = document.getElementById("seconds")
 const ampmEl = document.getElementById("ampm")
+
+function updateClock() {
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    
+}
