b8a0f1850e57c07bf72a075323de7b0ac07726ae
Anu <anupama12sg@gmail.com>
2025-06-22 18:25:19 +0530

    chore: const for hrs/mins/sec/ampm

---
+++ 9. digital-clock/index.js
@@ -0,0 +1,4 @@
+const hourEl = document.getElementById("hour")
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds")
+const ampmEl = document.getElementById("ampm")
