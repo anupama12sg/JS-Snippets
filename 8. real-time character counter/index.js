3ea3ec005bb5fdec7df0a27ea884f8b61ec8addd
Anu <anupama12sg@gmail.com>
2025-06-22 16:26:59 +0530

    chore: updateCounter() working

--- 8. real-time character counter/index.js
+++ 8. real-time character counter/index.js
@@ -1,9 +1,10 @@
 const textAreaEl = document.getElementById("textarea")
+const totalCounterEl = document.getElementById("total-counter")
 
 textAreaEl.addEventListener("keyup", () => {
     updateCounter()
 })
 
 function updateCounter() {
-
+    totalCounterEl.innerText = textAreaEl.value.length
 }
