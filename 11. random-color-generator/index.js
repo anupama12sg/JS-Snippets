2b10f2783afa22c46a60e59f6003a67e44a72aeb
Anu <anupama12sg@gmail.com>
2025-06-25 15:27:06 +0530

    chore: add several functions for color change

--- 11. random-color-generator/index.js
+++ 11. random-color-generator/index.js
@@ -6,12 +6,24 @@ for (let index = 0; index < 30; index++) {
     containerEl.appendChild(colorContainerEl);
 }
 
+const colorContainerEls = document.querySelectorAll(".color-container")
+
+generateColors();
+
+function generateColors() {
+    colorContainerEls.forEach((colorContainerEl) => {
+        const newColorCode = randomColor()
+        colorContainerEl.style.backgroundColor = "#" + newColorCode;
+    })
+}
+
 function randomColor() {
     const chars = "0123456789abcdef"
     const colorCodeLength = 6;
-    let color = ""
+    let colorCode = "";
     for (let index = 0; index < colorCodeLength; index++) {
-        
-
+        const randomNum = Math.floor(Math.random() * chars.length)
+        colorCode += chars.substring(randomNum, randomNum + 1)
     }
+    return colorCode
 }
