0fcaf466cee074de4153de9145e3d363708dc26a
Anu <anupama12sg@gmail.com>
2025-06-29 20:20:03 +0530

    chore: const imgContainerEl, prevEl, nextEl & an eventListener

---
+++ 12. rotating-image-gallery/index.js
@@ -0,0 +1,8 @@
+const imageContainerEl = document.querySelector(".img-container")
+
+const prevEl = document.getElementById("prev")
+const nextEl = document.getElementById("next")
+
+prevEl.addEventListener("click", () => {
+
+})
