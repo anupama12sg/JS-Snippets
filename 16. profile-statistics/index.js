68ae4c0b6931b3de64056eb3b397e1b59da24b4d
Anu <anupama12sg@gmail.com>
2025-07-06 12:59:09 +0530

    chore: create a function, added currentNum & dataCeil

--- 16. profile-statistics/index.js
+++ 16. profile-statistics/index.js
@@ -1,5 +1,12 @@
 const countersEl = document.querySelectorAll(".counter")
 
 countersEl.forEach(countersEl => {
-    countersEl.innerText = "0"
-})
+    countersEl.innerText = "0";
+
+    incrementCounter()
+
+    function incrementCounter() {
+        const currentNum = +countersEl.innerHTML
+        const dataCeil = countersEl.getAttribute("data-ceil")
+    }
+});
