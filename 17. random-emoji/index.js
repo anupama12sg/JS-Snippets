ae0164b103a1caf0c2e44b0cc0cf3ce811bbd9a7
Anu <anupama12sg@gmail.com>
2025-07-08 15:51:24 +0530

    chore: used api key for emojis

--- 17. random-emoji/index.js
+++ 17. random-emoji/index.js
@@ -5,6 +5,9 @@ const emoji = [];
 
 async function getEmoji() {
     let response = await fetch("https://emoji-api.com/emojis?access_key=061c1b8aa901b2b2b14edbcda506481d2182156a")
+
+    data = await response.json()
+    console.log(data)
 }
 
 getEmoji()
