3601e1352cd74c531751e8173142494656c0dd60
Anu <anupama12sg@gmail.com>
2025-07-08 15:37:53 +0530

    chore: use of async & await

--- 17. random-emoji/index.js
+++ 17. random-emoji/index.js
@@ -3,12 +3,12 @@ const emojiNameEl = document.getElementById("emoji-name")
 
 const emoji = [];
 
-function getEmoji() {
-
+async function getEmoji() {
+    let response = await fetch("https://emoji-api.com/emojis?access_key=061c1b8aa901b2b2b14edbcda506481d2182156a")
 }
 
 getEmoji()
 
 btnEl.addEventListener("click", () => {
-    
+
 })
