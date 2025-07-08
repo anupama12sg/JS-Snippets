eb7ce57bbf1b6dfbfcd2cdd69905949cf77b1e99
Anu <anupama12sg@gmail.com>
2025-07-08 16:02:43 +0530

    chore: changes through innerText

--- 17. random-emoji/index.js
+++ 17. random-emoji/index.js
@@ -7,11 +7,22 @@ async function getEmoji() {
     let response = await fetch("https://emoji-api.com/emojis?access_key=061c1b8aa901b2b2b14edbcda506481d2182156a")
 
     data = await response.json()
+
     console.log(data)
+
+    for (let i = 0; i < 1500; i++) {
+        emoji.push({
+            emojiName: data[i].character,
+            emojiCode: data[i].unicodeName
+
+        })
+    }
 }
 
 getEmoji()
 
 btnEl.addEventListener("click", () => {
-
+    const randomNum = Math.floor(Math.random() * emoji.length);
+    btnEl.innerText = emoji[randomNum].emojiName
+    emojiNameEl.innerText = emoji[randomNum].emojiCode
 })
