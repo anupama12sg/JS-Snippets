8b178ea08a8429605d3a95b27541cb38aa452385
Anu <anupama12sg@gmail.com>
2025-07-22 14:03:31 +0530

    chore: created updateCountdown()

--- 19. new-year-countdown/index.js
+++ 19. new-year-countdown/index.js
@@ -2,3 +2,11 @@ const dayEl = document.getElementById("day")
 const hourEl = document.getElementById("hour")
 const minuteEl = document.getElementById("minute")
 const secondEl = document.getElementById("second")
+
+const newYearDate = new Date("Jan 1, 2025 00:00:00").getTime()
+
+updateCountdown()
+
+function updateCountdown() {
+
+}
